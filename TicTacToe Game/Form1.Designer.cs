ee33c0d5d990624f8aaf2799471d3bcde67b1e39
unknown <urielryanreyles@gmail.com>
2022-06-02 23:43:02 +0800

    The Start

---
+++ TicTacToe Game/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TicTacToe_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.U001 = new System.Windows.Forms.Button();
+            this.U002 = new System.Windows.Forms.Button();
+            this.U003 = new System.Windows.Forms.Button();
+            this.R001 = new System.Windows.Forms.Button();
+            this.R002 = new System.Windows.Forms.Button();
+            this.R003 = new System.Windows.Forms.Button();
+            this.I001 = new System.Windows.Forms.Button();
+            this.I002 = new System.Windows.Forms.Button();
+            this.I003 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // U001
+            // 
+            this.U001.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.U001.Location = new System.Drawing.Point(64, 60);
+            this.U001.Name = "U001";
+            this.U001.Size = new System.Drawing.Size(186, 186);
+            this.U001.TabIndex = 0;
+            this.U001.UseVisualStyleBackColor = true;
+            // 
+            // U002
+            // 
+            this.U002.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.U002.Location = new System.Drawing.Point(249, 60);
+            this.U002.Name = "U002";
+            this.U002.Size = new System.Drawing.Size(186, 186);
+            this.U002.TabIndex = 1;
+            this.U002.UseVisualStyleBackColor = true;
+            // 
+            // U003
+            // 
+            this.U003.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.U003.Location = new System.Drawing.Point(434, 60);
+            this.U003.Name = "U003";
+            this.U003.Size = new System.Drawing.Size(186, 186);
+            this.U003.TabIndex = 2;
+            this.U003.UseVisualStyleBackColor = true;
+            // 
+            // R001
+            // 
+            this.R001.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R001.Location = new System.Drawing.Point(64, 245);
+            this.R001.Name = "R001";
+            this.R001.Size = new System.Drawing.Size(186, 186);
+            this.R001.TabIndex = 3;
+            this.R001.UseVisualStyleBackColor = true;
+            // 
+            // R002
+            // 
+            this.R002.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R002.Location = new System.Drawing.Point(249, 245);
+            this.R002.Name = "R002";
+            this.R002.Size = new System.Drawing.Size(186, 186);
+            this.R002.TabIndex = 4;
+            this.R002.UseVisualStyleBackColor = true;
+            // 
+            // R003
+            // 
+            this.R003.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.R003.Location = new System.Drawing.Point(434, 245);
+            this.R003.Name = "R003";
+            this.R003.Size = new System.Drawing.Size(186, 186);
+            this.R003.TabIndex = 5;
+            this.R003.UseVisualStyleBackColor = true;
+            // 
+            // I001
+            // 
+            this.I001.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.I001.Location = new System.Drawing.Point(64, 430);
+            this.I001.Name = "I001";
+            this.I001.Size = new System.Drawing.Size(186, 186);
+            this.I001.TabIndex = 6;
+            this.I001.UseVisualStyleBackColor = true;
+            // 
+            // I002
+            // 
+            this.I002.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.I002.Location = new System.Drawing.Point(249, 430);
+            this.I002.Name = "I002";
+            this.I002.Size = new System.Drawing.Size(186, 186);
+            this.I002.TabIndex = 7;
+            this.I002.UseVisualStyleBackColor = true;
+            // 
+            // I003
+            // 
+            this.I003.Font = new System.Drawing.Font("Arial Rounded MT Bold", 68.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.I003.Location = new System.Drawing.Point(434, 430);
+            this.I003.Name = "I003";
+            this.I003.Size = new System.Drawing.Size(186, 186);
+            this.I003.TabIndex = 8;
+            this.I003.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(721, 687);
+            this.Controls.Add(this.I003);
+            this.Controls.Add(this.I002);
+            this.Controls.Add(this.I001);
+            this.Controls.Add(this.R003);
+            this.Controls.Add(this.R002);
+            this.Controls.Add(this.R001);
+            this.Controls.Add(this.U003);
+            this.Controls.Add(this.U002);
+            this.Controls.Add(this.U001);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacGo Go Go!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button U001;
+        private Button U002;
+        private Button U003;
+        private Button R001;
+        private Button R002;
+        private Button R003;
+        private Button I001;
+        private Button I002;
+        private Button I003;
+    }
+}
